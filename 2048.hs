1db64e3ce713a8abb85ec88aa51a13e7dacda94d
Kristian Hentschel <khentschel@gmail.com>
2014-04-05 12:58:48 +0100

    changed explicit positions to matrix notation for grid representation. implemented originally specified conversion functions to lists with matrix transpose.

--- 2048.hs
+++ 2048.hs
@@ -1,22 +1,9 @@
 module Main where
+import Data.List
 
-
-data Position = Position Integer Integer deriving (Show, Read)
-data Tile = TEmpty | TNum Integer deriving (Show, Read, Eq)
-data Direction = MoveUp | MoveDown | MoveLeft | MoveRight
-data Grid = Grid Integer [(Position, Tile)]
-
--- Find the position moved towards for the given position.
-adjacentPos :: Grid -> Position -> Direction -> Maybe Position
-adjacentPos (Grid size _) (Position x y) dir = 
-    let
-        (x', y') = case dir of
-            MoveUp      -> (x,     y - 1)
-            MoveDown    -> (x,     y + 1)
-            MoveLeft    -> (x - 1, y)
-            MoveRight   -> (x + 1, y)
-    in
-        if (x < size && x >= 0 && y < size && y >= 0) then Just $ Position x' y' else Nothing
+data Tile = TEmpty | TNum Int deriving (Show, Read, Eq)
+data Direction = MoveUp | MoveDown | MoveLeft | MoveRight deriving (Show, Read, Eq)
+data Grid = Grid Int [[Tile]] deriving (Show, Read, Eq)
 
 -- Apply the 2048 rules to a single list of tiles representing a row or column, moving towards the first element.
 moveList :: [Tile] -> [Tile]
@@ -32,6 +19,48 @@ moveList (x:y:xs)       =
           TNum numx = x
 moveList (x:xs)         = (x:xs)
 
+-- Convert the Grid into a list of columns or rows, starting with the edge moved towards
+gridToLists :: Direction -> Grid -> [[Tile]]
+gridToLists dir (Grid size tiles) =
+    case dir of
+        MoveUp    -> transpose tiles
+        MoveDown  -> map reverse $ transpose tiles
+        MoveLeft  -> tiles
+        MoveRight -> map reverse tiles
+
+-- Create an empty grid for the start of the game
+emptyGrid :: Int -> Grid
+emptyGrid size = Grid size tiles
+    where tiles = take size $ repeat $ take size $ repeat TEmpty
+
+-- Restore the Grid from the lists of Tiles
+listsToGrid :: Direction -> [[Tile]] -> Grid
+listsToGrid dir tiles = Grid (length tiles) tiles'
+    where tiles' = case dir of
+            MoveUp    -> transpose tiles
+            MoveDown  -> map reverse $ transpose tiles
+            MoveLeft  -> tiles
+            MoveRight -> map reverse tiles
+
+-- replace a random empty tile in the grid with a 2 (90%) or 4 (10%) tile
+addRandom :: Grid -> Grid
+addRandom grid = grid
+
+-- Do the move, and if the grid changed, check if the game is lost or add a random new tile.
+move :: Direction -> Grid -> Grid
+move dir grid = grid'
+    where grid' = ((listsToGrid dir) . (map moveList) . (gridToLists dir)) grid
+
+-- user io
+main :: IO ()
+main = return()
+
+
+
+
+
+
+
 -- some test cases for manual inspection in ghci. TODO port to HUnit or similar.
 testMoveList :: ([Tile] -> [Tile]) -> [[Tile]]
 testMoveList f =
@@ -46,23 +75,3 @@ testMoveList f =
     in
         map f tests
 
--- Convert the Grid into a list of columns or rows, starting with the edge moved towards
-gridToLists :: Direction -> Grid -> [[Tile]]
-gridToLists dir grid = [[]]
-
--- Restore the Grid from the lists of Tiles
-listsToGrid :: Direction -> [[Tile]] -> Grid
-listsToGrid dir tiles = Grid 0 []
-
--- replace a random empty tile in the grid with a 2 (90%) or 4 (10%) tile
-addRandom :: Grid -> Grid
-addRandom grid = grid
-
--- Do the move, and if the grid changed, check if the game is lost or add a random new tile.
-move :: Grid -> Direction -> Grid
-move grid dir = grid'
-    where grid' = ((listsToGrid dir). (map moveList) . (gridToLists dir)) grid
-
--- user io
-main :: IO ()
-main = return()
